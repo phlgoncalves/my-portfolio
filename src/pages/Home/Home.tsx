b8e19e6843c516bb4fe59efb7603fedd9208083e
Pedro Goncalves <peedro.goncalves@gmail.com>
2024-07-28 17:30:51 -0300

    feat: created Hero component UI

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,14 @@
+import Navbar from "../../components/Navbar/Navbar";
+import Hero from "./sections/Hero/Hero";
+
+const Home = () => {
+
+  return (
+    <>
+      <Navbar />
+      <Hero />
+    </>
+  )
+}
+
+export default Home
